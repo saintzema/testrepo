7741724849f5f8ea6d031c3a898a3f17a3879147
Saint Zema <emezeji98@gmail.com>
2024-06-21 09:18:33 -0400

    First Commit

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,43 @@
+let userRole = "Manager";
+let accesslevel;
+let isLoggedIn = true;
+let userMessage;
+let userType = "subscriber";
+let userCategory;
+
+if (userRole === "admin") {
+    accesslevel = "Full access granted";
+} else if (userRole === "manager") {
+    accesslevel = "Limited access granted";
+} else {
+    accesslevel = "No access granted";
+}
+
+console.log("Access Level:", accesslevel);
+if (isLoggedIn) {
+    if(userRole === "admin") {
+    userMessage = "Welcome, Admin!";
+    } else if(userRole === "User") {
+        userMessage = "Welcome, User!";
+    } else {
+        userMessage = "Please log in to access the system";
+    }
+}
+console.log("User Message: ", userMessage);
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    case "user":
+        userCategory = "User";
+    default:
+        userCategory = "Unknown";
+}
+console.log("The User Category is: ", userCategory);
