9e0ea067b8d7f279402688799abcf49d442700fb
SaintZema <emezeji98@gmail.com>
2024-06-21 04:29:35 -0400

    First Commit

---
+++ SampleFolder/Scope/scope_lab.js
@@ -0,0 +1,26 @@
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    //
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
+console.log(blockLet);
+
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+
+show();
+console.log(functionVar);
+//console.log(functionLet);
+//console.log(functionConst);
